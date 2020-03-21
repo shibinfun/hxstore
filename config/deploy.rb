ed6d62944a1353b68f08b6fb2e00a7d7c39d7566
shibin <910872228@qq.com>
2020-03-21 09:26:26 +0800

    3

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
+set :passenger_restart_with_touch, true
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
